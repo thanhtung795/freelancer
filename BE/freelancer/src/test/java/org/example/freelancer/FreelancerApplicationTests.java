0ea23106709b12e97b7edf8bfdaff833d46b3597
thanhtung795 <vothanhtung795@gmail.com>
2024-09-16 22:15:39 +0700

    first commit

---
+++ BE/freelancer/src/test/java/org/example/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
