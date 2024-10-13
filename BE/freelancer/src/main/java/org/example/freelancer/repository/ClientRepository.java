0599316c93678e7039055f619cee03f01e856a50
Tên dquangv <dquangvu7998@gmail.com>
2024-10-13 22:11:59 +0700

    add company client by id api

--- BE/freelancer/src/main/java/org/example/freelancer/repository/ClientRepository.java
+++ BE/freelancer/src/main/java/org/example/freelancer/repository/ClientRepository.java
@@ -4,9 +4,11 @@ import org.example.freelancer.dto.ClientCompanyDTO;
 import org.example.freelancer.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
@@ -22,5 +24,12 @@ public interface ClientRepository extends JpaRepository<Client, Integer> {
     List<Object[]> findClientWithCompanyDetails();
 
 
-
+    @Query("SELECT c.id, u.account.email, u.firstName, u.lastName, u.phoneNumber, "
+            + "co.id, co.companyName, co.phoneContact, co.address "
+            + "FROM Client c "
+            + "JOIN c.user u "
+            + "JOIN u.account a "
+            + "LEFT JOIN c.company co "
+            + "WHERE c.id = :clientId")
+    List<Object[]> findClientWithCompanyDetailsById(@Param("clientId") Integer clientId);
 }
