80b2480f7f58827fda7d00b187fccd63f892fbe8
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-10-14 13:33:53 +0700

    Fix API AllJobNames

--- BE/freelancer/src/main/java/org/example/freelancer/dto/JobNameCategoryAndClientDTO.java
+++ BE/freelancer/src/main/java/org/example/freelancer/dto/JobNameCategoryAndClientDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 public class JobNameCategoryAndClientDTO {
+    private Integer clientId;
     private Integer id;
     private String title;
     private String scope;
