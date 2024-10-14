b592ae6e3b631c21da5e34c21aec59337951ee66
tiendinh203 <tientc0909@gmail.com>
2024-10-14 19:43:33 +0700

    c

--- FE/freelance/src/component/ListJobUploaded/ListJobUploaded.js
+++ FE/freelance/src/component/ListJobUploaded/ListJobUploaded.js
@@ -19,110 +19,38 @@ import {
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
-const allJobs = [
-  {
-    id: 1,
-    clientId: 1,
-    title: "Website Development",
-    scope: "large",
-    hourWork: 150.0,
-    jobOpportunity: true,
-    fromPrice: 1000.0,
-    toPrice: 5000.0,
-    typePrice: "fixed",
-    status: "Hủy bỏ",
-    dateStart: "2024-01-01T00:00:00",
-    dateEnd: "2024-02-02T00:00:00",
-    dateCreate: null,
-    firstName: "Bob",
-    lastName: "Johnson",
-    categoryName: "Web Development",
-    skills: ["JavaScript", "Python"],
-  },
-  {
-    id: 2,
-    clientId: 2,
-    title: "Mobile App Development",
-    scope: "medium",
-    hourWork: 120.0,
-    jobOpportunity: true,
-    fromPrice: 800.0,
-    toPrice: 3000.0,
-    typePrice: "fixed",
-    status: "Đang hoạt động",
-    dateStart: "2024-03-01T00:00:00",
-    dateEnd: "2024-04-01T00:00:00",
-    dateCreate: null,
-    firstName: "Alice",
-    lastName: "Smith",
-    categoryName: "Mobile Development",
-    skills: ["React Native", "Java"],
-  },
-  {
-    id: 3,
-    clientId: 1,
-    title: "E-commerce Website",
-    scope: "large",
-    hourWork: 200.0,
-    jobOpportunity: true,
-    fromPrice: 1500.0,
-    toPrice: 7000.0,
-    typePrice: "fixed",
-    status: "Đang hoạt động",
-    dateStart: "2024-02-10T00:00:00",
-    dateEnd: "2024-03-10T00:00:00",
-    dateCreate: null,
-    firstName: "Charlie",
-    lastName: "Brown",
-    categoryName: "Web Development",
-    skills: ["PHP", "Laravel"],
-  },
-  {
-    id: 4,
-    clientId: 3,
-    title: "SEO Optimization",
-    scope: "small",
-    hourWork: 100.0,
-    jobOpportunity: true,
-    fromPrice: 500.0,
-    toPrice: 2500.0,
-    typePrice: "fixed",
-    status: "Hủy bỏ",
-    dateStart: "2024-05-01T00:00:00",
-    dateEnd: "2024-06-01T00:00:00",
-    dateCreate: null,
-    firstName: "David",
-    lastName: "Williams",
-    categoryName: "Digital Marketing",
-    skills: ["SEO", "Content Marketing"],
-  },
-  
-];
-
 const ListJobUploaded = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 2;
   const [jobs, setJobs] = useState([]);
   const navigate = useNavigate();
+  const role = JSON.parse(localStorage.getItem("user"))?.data?.role == "client";
 
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const clientId = parseInt(JSON.parse(localStorage.getItem('user')).data.idRole, 10);
+        const clientId = parseInt(
+          JSON.parse(localStorage.getItem("user")).data.idRole,
+          10
+        );
 
-        const response = await fetch("http://localhost:8080/api/Jobs/getAllJobName");
+        const response = await fetch(
+          "http://localhost:8080/api/Jobs/getAllJobName"
+        );
         const data = await response.json();
-        console.log('clientId ', clientId)
-        console.log('data ', data.data)
-        
-        const filteredJobs = data.data.filter((job) => job.clientId === clientId);
+        console.log("clientId ", clientId);
+        console.log("data ", data.data);
+
+        const filteredJobs = data.data.filter(
+          (job) => job.clientId === clientId
+        );
         setJobs(filteredJobs);
       } catch (error) {
         console.error("Error fetching jobs:", error);
       }
     };
 
-    fetchJobs()
+    fetchJobs();
   }, []);
 
   const handleDelete = (title) => {
@@ -175,13 +103,17 @@ const ListJobUploaded = () => {
             <List.Item>
               <Card title={job.title} className="job-card">
                 <p>
-                  <strong>Price:</strong> {job.fromPrice} - {job.toPrice}
+                  <strong>Lương: </strong> {job.fromPrice} - {job.toPrice}
                 </p>
                 <p>
-                  <strong>Work Time:</strong> {job.hourWork} hours
+                  <strong>Thời gian làm việc: </strong> {job.hourWork} hours
                 </p>
                 <p>
-                  <strong>Status:</strong> {job.status}
+                  <strong>Trạng thái: </strong> {job.status}
+                </p>
+                <p>
+                  <strong>Họ và tên: </strong>{" "}
+                  {job.fristName + " " + job.lasName}
                 </p>
                 <Button
                   type="primary"
@@ -233,51 +165,55 @@ const ListJobUploaded = () => {
           style={{ textAlign: "center", marginTop: 16 }}
         />
       </Col>
-      <Col span={6} style={{ textAlign: "center" }}>
-        <div className="stats">
-          <h3>Total Jobs: {jobs.length}</h3>
-          <div style={{ marginTop: 16 }} className="d-flex flex-column gap-2">
-            <Button
-              type="primary"
-              icon={<FontAwesomeIcon icon={faPlus} />}
-              onClick={handleAddJob}
-              style={{
-                width: "100%",
-                backgroundColor: "#4CAF50",
-                borderColor: "#4CAF50",
-              }}
-            >
-              Add Job
-            </Button>
-            <Button
-              type="danger"
-              icon={<FontAwesomeIcon icon={faArchive} />}
-              style={{
-                width: "100%",
-                backgroundColor: "gray",
-                borderColor: "#2196F3",
-                color: "white",
-              }}
-              onClick={handleNavigateArchive}
-            >
-              View Archived Jobs
-            </Button>
-            <Button
-              type="danger"
-              icon={<FontAwesomeIcon icon={faTrash} />}
-              style={{
-                width: "100%",
-                backgroundColor: "#F44336",
-                borderColor: "#F44336",
-                color: "white",
-              }}
-              onClick={handleNavigateDelete}
-            >
-              View Deleted Jobs
-            </Button>
+      {role ? (
+        <Col span={6} style={{ textAlign: "center" }}>
+          <div className="stats">
+            <h3>Total Jobs: {jobs.length}</h3>
+            <div style={{ marginTop: 16 }} className="d-flex flex-column gap-2">
+              <Button
+                type="primary"
+                icon={<FontAwesomeIcon icon={faPlus} />}
+                onClick={handleAddJob}
+                style={{
+                  width: "100%",
+                  backgroundColor: "#4CAF50",
+                  borderColor: "#4CAF50",
+                }}
+              >
+                Add Job
+              </Button>
+              <Button
+                type="danger"
+                icon={<FontAwesomeIcon icon={faArchive} />}
+                style={{
+                  width: "100%",
+                  backgroundColor: "gray",
+                  borderColor: "#2196F3",
+                  color: "white",
+                }}
+                onClick={handleNavigateArchive}
+              >
+                View Archived Jobs
+              </Button>
+              <Button
+                type="danger"
+                icon={<FontAwesomeIcon icon={faTrash} />}
+                style={{
+                  width: "100%",
+                  backgroundColor: "#F44336",
+                  borderColor: "#F44336",
+                  color: "white",
+                }}
+                onClick={handleNavigateDelete}
+              >
+                View Deleted Jobs
+              </Button>
+            </div>
           </div>
-        </div>
-      </Col>
+        </Col>
+      ) : (
+        ""
+      )}
     </Row>
   );
 };
