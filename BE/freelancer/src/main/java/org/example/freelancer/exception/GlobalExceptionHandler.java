4393da1f9c83f146b0eb542a12af1fedaaa8935f
Tên dquangv <dquangvu7998@gmail.com>
2024-10-15 17:07:19 +0700

    add BadRequestException + accountUnitTest (Create, Update) - Mockito

--- BE/freelancer/src/main/java/org/example/freelancer/exception/GlobalExceptionHandler.java
+++ BE/freelancer/src/main/java/org/example/freelancer/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.example.freelancer.exception;
 
 import org.example.freelancer.dto.Response.Response;
+import org.example.freelancer.dto.ResponseObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -37,5 +39,63 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    /*@ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ResponseObject<Object>> handleUnauthorizedException(UnauthorizedException ex) {
+        ResponseObject<Object> responseObject = ResponseObject.builder()
+                .result(false)
+                .message("Unauthorized")
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .data(ex.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseObject);
+    }*/
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ResponseObject<Object>> handleBadException(BadRequestException ex) {
+        ResponseObject<Object> responseObject = ResponseObject.builder()
+                .result(false)
+                .message("Bad Request")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .data(ex.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest().body(responseObject);
+    }
+
+    /*@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InternalServerErrorException.class)
+    public ResponseEntity<ResponseObject<Object>> handleBadException(InternalServerErrorException ex) {
+        ResponseObject<Object> responseObject = ResponseObject.builder()
+                .result(false)
+                .message("Internal Server Error")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .data(ex.getMessage())
+                .build();
+
+        return ResponseEntity.internalServerError().body(responseObject);
+    }*/
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ResponseObject<Object>> handleNotFoundException(NotFoundException ex) {
+        ResponseObject<Object> responseObject = ResponseObject.builder()
+                .result(false)
+                .message("Not Found")
+                .status(HttpStatus.NOT_FOUND.value())
+                .data(ex.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseObject);
     }
 }
